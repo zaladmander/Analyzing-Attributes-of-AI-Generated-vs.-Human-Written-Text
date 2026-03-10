1406480b9b67a677d61d6d32e312b1ba377f1481
zaladmander <159731834+zaladmander@users.noreply.github.com>
2026-03-09 23:20:49 -0700

    just a teensy bit of etl

---
+++ src/etl.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from utils.paths import RAW_DATA_FILE
+
+verbose = bool(int(input("Verbose output? (0 or 1): ")))
+
+df = pd.read_csv(RAW_DATA_FILE)
+if verbose:
+    print("Raw data loaded from:", RAW_DATA_FILE)
+    print(df.head())
+
+df = (
+    df
+    .sort_values(by='id', ascending=True)
+    .reset_index(drop=True)
+)
+if verbose:
+    print("Data after sorting and resetting index:")
+    print(df.head())
