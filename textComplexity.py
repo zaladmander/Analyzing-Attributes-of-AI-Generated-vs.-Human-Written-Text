ecf737f77fdf316cec34c62fd1c3d45b3649e4e1
somto04 <s.azubuike1@lancaster.ac.uk>
2026-03-18 16:08:21 -0700

    created sql statments to answer the text complexity question

--- textComplexity.py
+++ textComplexity.py
@@ -4,14 +4,65 @@ import pandas as pd
 
 connection = get_connection()
 
-textBySourceSql = f"""
-select label, topic, 
+def get_topic_data(topic):
+    topicsSql = f"""
+    select label, 
+    case when label = 'human' then 'human'
+    else source_detail
+    end as source_group,
+    AVG(length_chars) AS avg_chars,
+    AVG(length_words) AS avg_words, 
+    AVG(quality_score) AS avg_quality
+    from {TABLE_NAME}
+    where topic = '{topic}'
+    group by label, source_group"""
+
+    return pd.read_sql(topicsSql, connection)
+
+topics = [
+    "technology", "science", "sports", "health",
+    "education", "lifestyle", "food", "travel",
+    "finance", "entertainment"
+]
+
+topic_data = {}
+
+for topic in topics:
+    topic_data[topic] = get_topic_data(topic)
+
+# for human vs ai
+humanVsAiQuery = f"""
+select label, 
+AVG(length_chars) as avg_chars,
+AVG(length_words) as avg_words, 
+AVG(quality_score) as avg_quality
+from {TABLE_NAME}
+group by label"""
+
+humanVsAiData = pd.read_sql(humanVsAiQuery, connection)
+print(humanVsAiData)
+
+# for within topics 
+humanVsAiWithTopicsQuery = f"""
+select label, topic,
 AVG(length_chars) as avg_chars,
 AVG(length_words) as avg_words, 
 AVG(quality_score) as avg_quality
 from {TABLE_NAME}
 group by label, topic"""
 
-textBySourceData = pd.read_sql(textBySourceSql, connection)
+humanVsAiWithTopicsData = pd.read_sql(humanVsAiWithTopicsQuery, connection)
+print(humanVsAiWithTopicsData)
 
-print(textBySourceData.head())
+# for inter-ai analysis
+interAiQuery = f"""
+select label, source_detail,
+AVG(length_chars) as avg_chars,
+AVG(length_words) as avg_words, 
+AVG(quality_score) as avg_quality
+from {TABLE_NAME}
+where not label = 'human'
+group by label, source_detail"""
+
+interAiData = pd.read_sql(interAiQuery, connection)
+print(interAiData = pd.read_sql(interAiQuery, connection))
